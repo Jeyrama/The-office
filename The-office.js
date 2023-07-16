235db6be0255065e09eaad45073e0756fef9c146
Jey <126204426+Jeyrama@users.noreply.github.com>
2023-07-15 21:57:24 -0700

    Update The-office.js

--- The-office.js
+++ The-office.js
@@ -9,3 +9,6 @@ Happiness rating will be total score / number of people in the room.
 
 Note that your boss is in the room (boss), their score is worth double it's face value (but they are still just one person!).
 */
+
+
+// Solution
