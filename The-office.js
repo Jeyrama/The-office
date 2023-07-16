df718f773960a7c23c28875e780e2ce88f6b45db
Jey <126204426+Jeyrama@users.noreply.github.com>
2023-07-16 16:45:13 -0700

    Update The-office.js

--- The-office.js
+++ The-office.js
@@ -1,13 +1,21 @@
 /*
-Your colleagues have been looking over you shoulder. When you should have been doing your boring real job, you've been using the work computers to smash in endless hours of codewars.
+Your colleagues have been looking over your shoulder. 
+When you should have been doing your real job, 
+you've been using the work computers to go on leetcode.
 
-In a team meeting, a terrible, awful person declares to the group that you aren't working. You're in trouble. You quickly have to gauge the feeling in the room to decide whether or not you should gather your things and leave.
+In a team meeting, a person declares to the group that you aren't working. 
+You're in trouble. You quickly have to gauge the feeling in the room to 
+decide whether or not you should gather your things and leave.
 
-Given an object (meet) containing team member names as keys, and their happiness rating out of 10 as the value, you need to assess the overall happiness rating of the group. If <= 5, return 'Get Out Now!'. Else return 'Nice Work Champ!'.
+Given an object (meet) containing team member names as keys, 
+and their happiness rating out of 10 as the value, 
+you need to assess the overall happiness rating of the group. 
 
+If <= 5, return 'Get Out Now!'. Else return 'Nice Work Champ!'.
 Happiness rating will be total score / number of people in the room.
 
-Note that your boss is in the room (boss), their score is worth double it's face value (but they are still just one person!).
+Note: that your boss is in the room (boss), 
+their score is worth double it's face value (but they are still just one person!).
 */
 
 
@@ -23,17 +31,17 @@ function outed(meet, boss) {
 
 function outed(meet, boss){
 
-  let totalHappyness = 0
+  let totalHappiness = 0
   
   for (item in meet){
     if (item === boss) {
-      totalHappyness += (meet[item] *2) 
-    } else { totalHappyness += meet[item]}
+      totalHappiness += (meet[item] *2) 
+    } else { totalHappiness += meet[item]}
   }
-  let averageHappyness = totalHappyness / Object.keys(meet).length 
+  let averageHappiness = totalHappiness / Object.keys(meet).length 
   
   
-  if (averageHappyness <= 5) {
+  if (averageHappiness <= 5) {
     return "Get Out Now!"
   } else {return "Nice Work Champ!"}
    
