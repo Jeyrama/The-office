8ea096c4c21c70f537ffcdd32527a203479f4f3c
Jey <126204426+Jeyrama@users.noreply.github.com>
2023-07-15 21:57:52 -0700

    Update The-office.js

--- The-office.js
+++ The-office.js
@@ -12,3 +12,9 @@ Note that your boss is in the room (boss), their score is worth double it's face
 
 
 // Solution
+
+function outed(meet, boss) {
+  let names = Object.keys(meet)
+  let score = names.reduce((s,v) => s + meet[v], 0) + meet[boss]
+  return score / names.length > 5 ? 'Nice Work Champ!' : 'Get Out Now!'
+}
